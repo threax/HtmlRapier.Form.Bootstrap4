33d2ee052b7d003c7b39de303beeba7844633429
Andrew Piper <andrew@threax.com>
2021-02-22 09:52:18 -0500

    Added tags to push script.

--- Push.ps1
+++ Push.ps1
@@ -1,4 +1,8 @@
 git push
+git push --tags
 git push https://github.com/threax/HtmlRapier.Form.Bootstrap4.git
+git push https://github.com/threax/HtmlRapier.Form.Bootstrap4.git --tags
 git push https://gitlab.com/threax-software/HtmlRapier.Form.Bootstrap4.git
+git push https://gitlab.com/threax-software/HtmlRapier.Form.Bootstrap4.git --tags
 git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier.Form.Bootstrap4
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier.Form.Bootstrap4 --tags
