f957a45ff11ceaf90c51f5bd6874ed5d1e9eff96
Andrew Piper <andrew@threax.com>
2021-02-22 09:48:53 -0500

    Added push file.

---
+++ Push.ps1
@@ -0,0 +1,4 @@
+git push
+git push https://github.com/threax/HtmlRapier.Form.Bootstrap4.git
+git push https://gitlab.com/threax-software/HtmlRapier.Form.Bootstrap4.git
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier.Form.Bootstrap4
